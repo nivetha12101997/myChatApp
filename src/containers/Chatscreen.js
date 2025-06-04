95f2fcda0f551819218a4547ca039439920623a6
nivetha <nivethabe1997@gmail.com>
2025-06-04 20:08:01 +0530

    Added Chat Screen

--- src/containers/Chatscreen.js
+++ src/containers/Chatscreen.js
@@ -1,11 +1,47 @@
-import {View ,Text,TextInput,TouchableOpacity,StyleSheet} from 'react-native';
-import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { styles } from '../styles/Chatscreen_styles.js';
+import { chatData } from '../dummyData/ChatData.js';
 
 
-export const Chatscreen =()=>{
+export const Chatscreen = (props) => {
+    console.log("ChatScreen props", props.route.params);
+    const { chatName } = props.route.params;
+    const [message, setMessage] = useState('');
+        useEffect(() => {
+        console.log(chatName);
+        const filteredMessage= chatData.filter((item) => item.name === chatName);
+        
+        setMessage(filteredMessage[0].messages);
+        console.log("Filtered Messages:", filteredMessage);
+    }, [chatName]);
+
+    const renderChatBubble = ({ item }) => {
+        console.log("Chat Bubble Item:", item);
+        return (
+            <View style={[styles.chatBubble,item.sender==='You' ? styles.sender : styles.receiver]} key={item.id}>
+                <Text>{item.text}</Text>
+                <Text style={{ fontSize: 10, color: 'gray' }}>{item.timestamp}</Text>
+            </View>
+        );
+    }
+
+
     return (
         <View>
-<Text>ChatScreen</Text>
+            {/* <Text>ChatScreen for {chatName} </Text> */}
+            <View>
+                <FlatList style={styles.chatScreen_container} data={message} renderItem={renderChatBubble} keyExtractor={(item,index) =>index } />
+            </View>
+            {/* <TextInput
+                style={styles.input}
+                placeholder="Type a message"
+                value={message}
+                onChangeText={setMessage}
+            />
+            <TouchableOpacity style={styles.button} onPress={() => console.log('Send message:', message)}>
+                <Text style={styles.buttonText}>Send</Text>
+            </TouchableOpacity> */}
         </View>
     )
 }
---
+++ src/styles/Chatscreen_styles.js
@@ -0,0 +1,27 @@
+import {View, Text,TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({  
+    chatBubble:{
+        maxWidth: '80%',
+         padding: 10,
+        borderRadius: 15,
+        marginVertical: 25,
+        backgroundColor:'blue',
+        marginHorizontal:12,
+
+    },
+    chatScreen_container:{
+        // display: 'flex',
+        // flexDirection: 'column',
+        // alignItems: 'center',
+        // justifyContent: 'start',
+    },
+    sender:{
+        alignSelf: 'flex-end',
+        backgroundColor: '#DCF8C6',
+    },
+    receiver:{
+        alignSelf: 'flex-start',
+        backgroundColor: '#ECE5DD',
+    },
+});

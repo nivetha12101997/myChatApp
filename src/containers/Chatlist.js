bc00ee5a480c0c92bdc5b89b941e4f88c4d7db2c
nivetha <nivethabe1997@gmail.com>
2025-06-04 20:07:45 +0530

    Added ChatList Page

--- src/containers/Chatlist.js
+++ src/containers/Chatlist.js
@@ -1,10 +1,34 @@
-import {View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList} from 'react-native';
 import React, {useState} from 'react';
+import {styles} from '../styles/Chatlist_styles.js';
+import { chatListData } from '../dummyData/ChatList.js';
+import { useNavigation } from '@react-navigation/native';
 
 export const Chatlist = () => {
+    const navigation = useNavigation();
+    const navigateToChatScreen = (chatName) => {
+        console.log(`Navigating to chat screen for: ${chatName}`);
+        navigation.navigate('ChatScreen', { chatName: chatName });
+    };
+    const renderData=({item,index})=>{
+        return(
+            <TouchableOpacity onPress={()=>navigateToChatScreen(item.name)}>
+            <View style={styles.chatlist_item}>
+                <Text style={styles.chatlist_item_text}>{item.name}</Text>
+
+            </View>
+            </TouchableOpacity>
+
+        )
+    }
     return(
-        <View>
-            <Text>ChatList Page</Text>
+        <View style={styles.chatlist_container}>
+            {/* <Text>ChatList Page</Text> */}
+            <FlatList
+            data={chatListData}
+            renderItem={renderData}
+            keyExtractor={(item, index) => index.toString()}
+            />
         </View>
     )
 }
---
+++ src/styles/Chatlist_styles.js
@@ -0,0 +1,34 @@
+import {Button, StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    chatlist_container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+    },
+    // chatlist_header: {
+    //     fontSize: 24,
+    //     fontWeight: 'bold',
+    //     marginBottom: 20,
+    // },
+    chatlist_item: {
+        padding: 15,
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        marginBottom: 10,
+    },
+    chatlist_item_text: {
+        fontSize: 18,
+    },
+    // new_chat_button: {
+    //     marginTop: 20,
+    //     backgroundColor: '#007BFF',
+    //     paddingVertical: 10,
+    //     borderRadius: 5,
+    // },
+    // new_chat_button_text: {
+    //     color: '#fff',
+    //     textAlign: 'center',
+    //     fontSize: 16,
+    // },
+});
